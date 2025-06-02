09397db96eebc2fce81967f3d49449db2fe4b39e
pallavi123695 <akuthotapallavi33@gmail.com>
2025-06-02 21:20:34 +0530

    Add files via upload

---
+++ SimpleRace.java
@@ -0,0 +1,37 @@
+package xyz;
+class Car extends Thread {
+	public void run() {
+		for(int i=1;i<=3;i++) {
+			System.out.println("BMW car is moving" + i);
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e) {
+		}
+	}
+}
+}
+class Bike extends Thread {
+	public void run() {
+		for(int i=1;i<=3;i++) {
+			System.out.println("Ninja Bike is moving " + i);
+			try {
+				Thread.sleep(500);
+			}catch(Exception e) {
+				
+			}
+		}
+		
+	}
+}
+public class SimpleRace {
+
+	public static void main(String[] args) {
+		
+		new Car().start();
+		new Bike().start();
+	}
+}
+
+	
+
