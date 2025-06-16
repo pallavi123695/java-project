f4eac66f10dfec487f64a7d444df2a88cc09b1f0
pallavi123695 <akuthotapallavi33@gmail.com>
2025-06-16 09:52:50 +0530

    Add files via upload

---
+++ FavoriteCandies.java
@@ -0,0 +1,16 @@
+
+package xyz;
+
+import java.util.LinkedHashSet;
+
+public class FavoriteCandies {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> candies=new LinkedHashSet<String>();
+		candies.add("Chocolate");
+		candies.add("Watermelon");
+		candies.add("Mango");
+		System.out.println(candies);
+	}
+
+}
